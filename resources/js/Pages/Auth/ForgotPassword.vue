0d930239e3780ad653225c422ebe91f7a0698754
paufrancisco <francisco.paulo.pf@gmail.com>
2026-03-03 06:39:04 +0800

    log in finish

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,68 +1,72 @@
+<template>
+  <GuestLayout>
+    <Head title="Forgot Password" />
+
+    <div class="mb-4 text-sm text-gray-600">
+      Forgot your password? No problem. Just let us know your email
+      address and we will email you a password reset link that will allow
+      you to choose a new one.
+    </div>
+
+    <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
+      {{ status }}
+    </div>
+
+    <form @submit.prevent="submit">
+      <div>
+        <InputLabel for="email" value="Email" />
+        <TextInput
+          id="email"
+          type="email"
+          class="mt-1 block w-full"
+          v-model="form.email"
+          required
+          autofocus
+          autocomplete="username"
+        />
+        <InputError class="mt-2" :message="form.errors.email" />
+      </div>
+
+      <div class="mt-4 flex items-center justify-between">
+        <!-- Back to Login -->
+        <Link
+          :href="route('login')"
+          class="text-sm text-gray-600 underline hover:text-gray-900"
+        >
+          Back to Login
+        </Link>
+
+        <!-- Submit Button -->
+        <PrimaryButton
+          :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing"
+        >
+          Email Password Reset Link
+        </PrimaryButton>
+      </div>
+    </form>
+  </GuestLayout>
+</template>
+
 <script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import InputError from '@/Components/InputError.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
+import TextInput from '@/Components/TextInput.vue'
+import { Head, Link, useForm } from '@inertiajs/vue3'
 
 defineProps({
-    status: {
-        type: String,
-    },
-});
+  status: {
+    type: String
+  }
+})
 
 const form = useForm({
-    email: '',
-});
+  email: ''
+})
 
 const submit = () => {
-    form.post(route('password.email'));
-};
+  form.post(route('password.email'))
+}
 </script>
-
-<template>
-    <GuestLayout>
-        <Head title="Forgot Password" />
-
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email
-            address and we will email you a password reset link that will allow
-            you to choose a new one.
-        </div>
-
-        <div
-            v-if="status"
-            class="mb-4 text-sm font-medium text-green-600"
-        >
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4 flex items-center justify-end">
-                <PrimaryButton
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
-</template>
