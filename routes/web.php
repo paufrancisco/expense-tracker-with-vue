0723ce577274a04db87f6be39da059ca2099a1a7
paufrancisco <francisco.paulo.pf@gmail.com>
2026-03-09 09:34:12 +0800

    clean web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-namespace Routes;
-
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,29 +14,25 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion'     => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/', fn () => redirect()->route('dashboard'))->name('home');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
-    Route::get('/transactions/export/csv', [TransactionController::class, 'export'])
+    Route::get('/transactions/export', [TransactionController::class, 'export'])
         ->name('transactions.export');
 
-    Route::resource('transactions', TransactionController::class);
+    Route::resource('transactions', TransactionController::class)
+        ->except(['show']);
 
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-
-    Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
-
-    Route::get('/', function () {
-        return redirect()->route('dashboard');
+    Route::prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
     });
 
 });
